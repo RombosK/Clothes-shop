def1415f17322ad24f0669010330186d5f05c11b
Roman <rombos1978@gmail.com>
2024-08-19 13:24:24 +0300

    Fix 19.08.24

--- slider/views.py
+++ slider/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Slider
+from store.models import Product
+
+def home(request):
+    slider_list = Slider.objects.all()
+    most_popular_products = Product.objects.filter(is_popular=True)  # Пример фильтрации популярных товаров
+    context = {
+        'slider_list': slider_list,
+        'most_popular_products': most_popular_products
+    }
+    return render(request, 'home.html', context)
