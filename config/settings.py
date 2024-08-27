af5fea6b9c3d845a9bdf5c9a6e5ddf4a9a6cefae
Roman <rombos1978@gmail.com>
2024-08-27 20:49:35 +0300

    updated

--- config/settings.py
+++ config/settings.py
@@ -157,7 +157,7 @@ if ENV_TYPE == 'local':
         BASE_DIR / 'static',
     ]
 else:
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # prod
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
